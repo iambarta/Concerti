5a564a9414e815c6d85afc13e8adda935c11a337
LoreArk <lorepusa95@gmail.com>
2025-05-09 10:02:59 +0200

    Initial commit

---
+++ concertportal/src/main/java/com/concertportal/concertportal/ConcertportalApplication.java
@@ -0,0 +1,13 @@
+package com.concertportal.concertportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcertportalApplication.class, args);
+	}
+
+}
---
+++ concertportal/src/test/java/com/concertportal/concertportal/ConcertportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.concertportal.concertportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
