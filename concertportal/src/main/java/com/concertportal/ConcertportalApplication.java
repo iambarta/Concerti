da685c7552e253fcf40aa97fee30e6f453ad4c0e
LoreArk <lorepusa95@gmail.com>
2025-05-09 12:42:20 +0200

    Project regenerated

--- concertportal/src/main/java/com/concertportal/ConcertportalApplication.java
+++ concertportal/src/main/java/com/concertportal/ConcertportalApplication.java
@@ -1,4 +1,4 @@
-package com.concertportal.concertportal;
+package com.concertportal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- concertportal/src/test/java/com/concertportal/ConcertportalApplicationTests.java
+++ concertportal/src/test/java/com/concertportal/ConcertportalApplicationTests.java
@@ -1,4 +1,4 @@
-package com.concertportal.concertportal;
+package com.concertportal;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
